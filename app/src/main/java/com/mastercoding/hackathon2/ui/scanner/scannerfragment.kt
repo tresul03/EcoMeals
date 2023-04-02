c72567d6b330acde978f61e016e46a86b5de8a1a
OliverMcEvoy <olivermcevoy11@gmail.com>
2023-04-02 09:28:16 +0100

    Adding the request for permissions

--- app/src/main/java/com/mastercoding/hackathon2/ui/scanner/scannerfragment.kt
+++ app/src/main/java/com/mastercoding/hackathon2/ui/scanner/scannerfragment.kt
@@ -1,10 +1,14 @@
 package com.mastercoding.hackathon2.ui.scanner
 
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.budiyev.android.codescanner.CodeScanner
 import com.budiyev.android.codescanner.CodeScannerView
@@ -13,19 +17,30 @@ import com.mastercoding.hackathon2.R
 
 class scannerfragment : Fragment() {
     private lateinit var codeScanner: CodeScanner
+    private lateinit var cameraButton: Button
+
+    companion object {
+        private const val CAMERA_PERMISSION_CODE = 100
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.fragment_scanner, container, false)
+        val view = inflater.inflate(R.layout.fragment_scanner, container, false)
+        cameraButton = view.findViewById<Button>(R.id.camera_permissions)
+
+        cameraButton.setOnClickListener {
+            checkPermission(android.Manifest.permission.CAMERA, CAMERA_PERMISSION_CODE)
+        }
+
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val scannerView = view.findViewById<CodeScannerView>(R.id.scanner_view)
-        val activity = requireActivity()
-        codeScanner = CodeScanner(activity, scannerView)
+        codeScanner = CodeScanner(requireActivity(), scannerView)
         codeScanner.decodeCallback = DecodeCallback {
-            activity.runOnUiThread {
-                Toast.makeText(activity, it.text, Toast.LENGTH_LONG).show()
+            requireActivity().runOnUiThread {
+                Toast.makeText(requireContext(), it.text, Toast.LENGTH_LONG).show()
             }
         }
         scannerView.setOnClickListener {
@@ -42,4 +57,36 @@ class scannerfragment : Fragment() {
         codeScanner.releaseResources()
         super.onPause()
     }
+
+    // Function to check and request permission.
+    private fun checkPermission(permission: String, requestCode: Int) {
+        if (ContextCompat.checkSelfPermission(requireContext(), permission) == PackageManager.PERMISSION_DENIED) {
+            // Requesting the permission
+            ActivityCompat.requestPermissions(requireActivity(), arrayOf(permission), requestCode)
+        } else {
+            Toast.makeText(requireContext(), "Permission already granted", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    // This function is called when the user accepts or decline the permission.
+    // Request Code is used to check which permission called this function.
+    // This request code is provided when the user is prompt for permission.
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == CAMERA_PERMISSION_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(requireContext(), "Camera Permission Granted", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(requireContext(), "Camera Permission Denied", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
 }
+// This function is called when the user accepts or decline the permission.
+// Request Code is used to check which permission called this function.
+// This request code is provided when the user is prompt for permissi
+
+
+
+
+
